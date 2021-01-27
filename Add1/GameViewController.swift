bb22c46d8d99ba4dbcebf7c34f53fae9b283369c
Tylor Schafer <tylorschafer@gmail.com>
2021-01-27 16:38:06 -0700

    Add update number and score functions

--- Add1/GameViewController.swift
+++ Add1/GameViewController.swift
@@ -12,6 +12,17 @@ class GameViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel?
     @IBOutlet weak var numberLabel: UILabel?
     @IBOutlet weak var inputField: UITextField?
+    
+    var score = 0
+    
+    func updateScoreLabel() {
+        scoreLabel?.text = String(score)
+    }
+    
+    func updateNumberLabel() {
+        numberLabel?.text = String.randomNumber(length: 4)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
